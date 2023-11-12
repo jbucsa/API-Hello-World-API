cd5307a8b6fd9d92421feca59d05e90162f4ac64
jbucsa <133785969+jbucsa@users.noreply.github.com>
2023-11-12 12:18:04 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 // DEPENDENCIES
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+);
 
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+
+// Languages: 
+const languagesController = require('./controllers/languages_controller.js');
+app.use('/languages', languagesController);
+
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
-})
+});
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('Greetings! From port: ', PORT);
-})
+});
